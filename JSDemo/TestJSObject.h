3f3696499b1ab3287dff47d24f2cf489530af228
otherplayer <osmk@qq.com>
2015-11-18 11:42:10 +0800

    first commit

---
+++ JSDemo/TestJSObject.h
@@ -0,0 +1,13 @@
+//
+//  TestJSObject.h
+//  JSDemo
+//
+//  Created by __无邪_ on 15/11/17.
+//  Copyright © 2015年 __无邪_. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestJSObject : NSObject
+
+@end
---
+++ JSDemo/TestJSObject.m
@@ -0,0 +1,13 @@
+//
+//  TestJSObject.m
+//  JSDemo
+//
+//  Created by __无邪_ on 15/11/17.
+//  Copyright © 2015年 __无邪_. All rights reserved.
+//
+
+#import "TestJSObject.h"
+
+@implementation TestJSObject
+
+@end
